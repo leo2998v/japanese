18455b8d69abcf23ea0ef1fc93b51ef198dfd575
leo2998v <leoveint98v@gmail.com>
2024-12-27 14:57:03 -0500

    Update submit.js

--- api/submit.js
+++ api/submit.js
@@ -1,12 +1,10 @@
-// api/submit.js
-
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     // Obtener datos enviados desde el frontend
-    const { score, level } = req.body;
+    const { score, level, userAnswer, correctAnswer } = req.body;
 
     // Aquí puedes manejar los datos como quieras (por ejemplo, guardarlos en una base de datos)
-    console.log(`Puntaje: ${score}, Nivel: ${level}`);
+    console.log(`Puntaje: ${score}, Nivel: ${level}, Respuesta escrita: ${userAnswer}, Respuesta correcta: ${correctAnswer}`);
 
     // Responder con un mensaje de éxito
     res.status(200).json({ message: 'Datos recibidos correctamente' });
